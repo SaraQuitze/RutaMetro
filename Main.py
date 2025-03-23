ce05f1cb9fd6eb01c4cdc1ce6d517c7d7e345519
Belladonna <102000211+SaraQuitze@users.noreply.github.com>
2025-03-23 16:16:51 -0500

    Update Main.py
    se añade una DB más robusta de estaciones de metro

--- Main.py
+++ Main.py
@@ -1,13 +1,28 @@
 import pandas as pd
-import numpy as np
+
 import networkx as nx
 
 
 #crear base de datos (las distancias no son reales)
 data = {
-    "Origen": ["Niquía", "Bello", "Madera", "San Javier", "Estadio", "Suramericana", "Poblado", "Aguacatala", "Itagüí", "Sabaneta"],
-    "Destino": ["Bello", "Madera", "San Javier", "Estadio", "Suramericana", "Poblado", "Aguacatala", "Itagüí", "Sabaneta", "La Estrella"],
-    "Distancia": [1500, 1200, 1800, 1300, 1100, 2000, 1400, 1600, 1200, 1000]
+    "Origen": [
+        "Niquía", "Bello", "Madera", "San Javier", "Estadio", "Suramericana", "Poblado", "Aguacatala", "Itagüí", "Sabaneta",
+        "San Antonio", "Alpujarra", "Cisneros", "Suramericana", "Estadio", "San Javier", "Poblado", "Aguacatala", "Itagüí", "Sabaneta",
+        "Acevedo", "Popular", "Santo Domingo Savio", "Andalucía", "San Javier", "Juan XXIII", "Miraflores", "El Poblado", "San Antonio", "Alpujarra",
+        "Cisneros", "Suramericana", "Estadio", "San Javier", "Poblado", "Aguacatala", "Itagüí", "Sabaneta"
+    ],
+    "Destino": [
+        "Bello", "Madera", "San Javier", "Estadio", "Suramericana", "Poblado", "Aguacatala", "Itagüí", "Sabaneta", "La Estrella",
+        "Alpujarra", "Cisneros", "Suramericana", "Estadio", "San Javier", "Poblado", "Aguacatala", "Itagüí", "Sabaneta", "La Estrella",
+        "Popular", "Santo Domingo Savio", "Andalucía", "La Aurora", "Juan XXIII", "Vallejuelos", "El Poblado", "San Antonio", "Alpujarra", "Cisneros",
+        "Suramericana", "Estadio", "San Javier", "Poblado", "Aguacatala", "Itagüí", "Sabaneta", "La Estrella"
+    ],
+    "Longitud de interestación": [
+        1500, 1200, 1800, 1300, 1100, 2000, 1400, 1600, 1200, 1000,
+        800, 900, 1100, 1300, 1200, 1400, 1500, 1600, 1200, 1000,
+        2000, 1800, 1500, 1200, 1300, 1400, 1600, 1200, 800, 900,
+        1100, 1300, 1200, 1400, 1500, 1600, 1200, 1000
+    ]
 }
 df = pd.DataFrame(data)
 
